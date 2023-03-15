af43e463ef17111a1a43ffd33dab55d65fe787eb
UnidentifiedX <80146445+UnidentifiedX@users.noreply.github.com>
2023-03-15 14:41:35 +0800

    rename directory

--- 8. Multiple particles with E _ B field/24Feb21/traj.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRAJ_H_INCLUDED
-#define TRAJ_H_INCLUDED
-
-using namespace std;
-void save_vti(string filename, int i,int n_space_div[3], double posl[3], double dd[3], uint64_t num,int ncomponents, double t, const char* data,string typeofdata);
-void save_pvd(string filename, int ndatapoints);
-void save_vtp(string filename, int i,  uint64_t num,int ncomponents, double t, const char* data, const char* points);
-#endif // TRAJ_H_INCLUDED
